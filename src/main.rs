6d1703976a644dc35d35eb91caca4e5ca3ccf6eb
kamei <leokamei1@gmail.com>
2025-10-31 15:00:40 -0300

    teste

--- src/main.rs
+++ src/main.rs
@@ -17,44 +17,63 @@ impl Config {
     }
 }
 
-struct Instruction {
-    name: String,
-    registers: (i32, i32, i32),
+enum Instruction {
+    add (i32, i32, i32),
+    sub (i32, i32, i32),
+    mul (i32, i32, i32),
+}
+
+enum Label {
+    string (String),
+    number (i32),
 }
 
 impl Instruction {
-    pub fn new(name: &str, registers: &str) {
-        Instruction { name.to_string(), }
-    }
-
-    fn parse_reg(registers: &str) {
-        for i in 0..registers.len() {
-            if registers[i] == ',' {
-
+    fn parse_reg(registers_string: &str) -> Result<(&str, Vec<Label>), &str> {
+        let regs: Vec<Label> = vec!();
+        let mut init = 0;
+        let mut last = 0;
+        while i < registers_string.len() {
+            if registers_string[i] == ',' {
+                match registers_string[init..last].parse::<i32>() {
+                    Ok(num) => regs.push(Label::number(num)),
+                    Err(_) => regs.push(Label::string(registers_string[init..last].clone())),
+                }
             }
+            i += 1;
         }
+        Ok(regs)
+    }
+
+    pub fn new(s_line: &str, idx_line: i32) -> Result<Instruction, &str> {
+        let regs = parse_line(s_line).unwrap();
+        let instruction: Instruction;
+        match name {
+            "add" => {
+                if regs.len() != 3 { return Err("Instrução linha {idx_line} errado!") }
+                instruction = Instruction::add(regs[0], regs[1], regs[2])
+                
+            },
+            "sub" => instruction = Instruction::sub(regs[0], regs[1], regs[2]),
+            "mul" => instruction = Instruction::mul(regs[0], regs[1], regs[2]),
+        }
+        Ok(instruction)
     }
 }
 
-fn create_instruction(name: &str, registers: &str) -> Instruction {
-
-}
-
-fn parser(&String code) {
-    let init = 0;
-    let last = 0;
-    let name: ;
-    let registers;
+fn parser(code: &String) {
+    let mut init = 0;
+    let mut last = 0;
+    let mut curr_line: &str;
+    let registers_string;
     let tokens: Vec<Instruction>;
+    let mut idx_line = 0;
     for i in 0..code.len() {
-        if code[i] == ' ' {
-            last = i;
-            name = &code[init..last];
-            init = i + 1;
-        } else if code[i] == '\n' {
-            registers = &code[init..last];
-            last = i;
-            tokens.push(create_instruction(name, registers));
+        if code[i] == '\n' {
+            last = i - 1;
+            curr_line = &code[init..last];
+            tokens.push(Instruction::new(curr_line, idx_line));
+            idx_line += 1;
             init = i + 1;
         }
     }
